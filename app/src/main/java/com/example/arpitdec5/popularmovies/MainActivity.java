32e407b7b187cf752057ac6adcbc1df83e795bbe
Arpit <arpitdec5@gmail.com>
2017-02-04 23:41:41 +0530

    made navigation drawer without drawer content

--- app/src/main/java/com/example/arpitdec5/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/arpitdec5/popularmovies/MainActivity.java
@@ -1,23 +1,44 @@
 package com.example.arpitdec5.popularmovies;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements MainActivityFragment.OnMovieClickListener{
 
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
     boolean two_pane = false;
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private SmoothActionBarDrawerToggle smoothActionBarToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        if(toolbar!=null) {
+            smoothActionBarToggle = new SmoothActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
+            ActionBar actionBar = getSupportActionBar();
+            drawerLayout.addDrawerListener(smoothActionBarToggle);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            smoothActionBarToggle.syncState();
+        }
 
         if(this.findViewById(R.id.movie_review_fragment)!=null)
         {
@@ -73,4 +94,37 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private class SmoothActionBarDrawerToggle extends ActionBarDrawerToggle {
+
+        private Runnable runnable;
+
+        public SmoothActionBarDrawerToggle(Activity activity, DrawerLayout drawerLayout, Toolbar toolbar, int openDrawerContentDescRes, int closeDrawerContentDescRes) {
+            super(activity, drawerLayout, toolbar, openDrawerContentDescRes, closeDrawerContentDescRes);
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            invalidateOptionsMenu();
+        }
+        @Override
+        public void onDrawerClosed(View view) {
+            super.onDrawerClosed(view);
+            invalidateOptionsMenu();
+        }
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            super.onDrawerStateChanged(newState);
+            if (runnable != null && newState == DrawerLayout.STATE_IDLE) {
+                runnable.run();
+                runnable = null;
+            }
+        }
+
+        public void runWhenIdle(Runnable runnable) {
+            this.runnable = runnable;
+        }
+    }
+
 }
